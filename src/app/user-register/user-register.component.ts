5cb2c80c2dce217d6bc30992d72cae8055418d76
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-07-08 20:26:27 +0400

    feat(forms-practices): In progress

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -12,22 +12,64 @@ export class UserRegisterComponent implements OnInit {
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.minLength(7)]],
-      confirmPassword: ['', [Validators.required, this.confirmPassword()]],
-      nickName: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
+      passwordGroup: fb.group({
+        password: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ._-]+$/), Validators.minLength(7)]],
+        confirmPassword: ['', Validators.required],
+      }, {
+          validators: this.checkPasswords
+        }),
+      nickName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9 ._-]+$/)]],
+      phoneNumber: ['', [Validators.required, this.validatorPhone]],
       website: ['', Validators.required],
       agreement: ['', Validators.required]
     })
   }
 
-  confirmPassword() {
-    return (formGroup) => {
-      return formGroup.password === formGroup.confirmPassword ? null : { notSame: { invalid: true } };
+  checkPasswords(formGroup) {
+    return formGroup.get('password').value === formGroup.get('confirmPassword').value ? null : { notSame: true };
+  }
+
+
+  ngOnInit() {
+  }
+
+  validatorPhone() {
+    return (formControl) => {
+      const value = formControl.value as string
+      return value.substr(0, 4) === '+380' && value.substring(4).length === 9 && /^\d+$/.test(value.substring(4)) ? null : { format: { invalid: true } };
     }
   }
 
-  ngOnInit() {
+  get email() {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.form.get('passwordGroup').get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.form.get('passwordGroup').get('confirmPassword') as FormControl;
+  }
+
+  get nickName() {
+    return this.form.get('nickName') as FormControl;
+  }
+
+  get phoneNumber() {
+    return this.form.get('phoneNumber') as FormControl;
+  }
+
+  get website() {
+    return this.form.get('website') as FormControl;
+  }
+
+  get agreement() {
+    return this.form.get('agreement') as FormControl;
+  }
+
+  get passwordGroup() {
+    return this.form.get('passwordGroup') as FormGroup;
   }
 
 }
